bbe4d3f0955c72af0dc8d779cf9f1704674c1fad
aishah-sha <n.aishahbahar@gmail.com>
2025-01-13 09:08:21 +0800

    Post new chats to Firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyA_13aQCnkHf3h9kX4QqjJVxCtxUT9BXBM",
     authDomain: "friendlychat-d0f26.firebaseapp.com",
-    projectId: "friendlychat-d0f26",'PROJECT_ID',
+    projectId: "friendlychat-d0f26",
     storageBucket: "friendlychat-d0f26.firebasestorage.app",
     messagingSenderId: "9078663734",
     appId: "1:9078663734:web:fdbb869f3bda5279dac697",
